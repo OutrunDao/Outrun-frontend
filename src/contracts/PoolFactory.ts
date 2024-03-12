b56427485abbc6f1e26851e02b0b927c1605db01
yx <liaoyux@gmail.com>
2024-03-12 17:22:52 +0800

    ug deps

---
+++ src/contracts/PoolFactory.ts
@@ -0,0 +1,5 @@
+export const name = 'PoolFactory';
+export const address = '0xA4867E7f3014eC398f9816049CBAc556Fa2a2ECd';
+export const abi = [
+  'function addLiquidity(address tokenA,address tokenB,uint amountADesired,uint amountBDesired,uint amountAMin,uint amountBMin,address to,uint deadline)',
+];
