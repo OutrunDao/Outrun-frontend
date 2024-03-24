3f20da78a06912c04b683e544a14341c1725f0d2
leacent <leason316@foxmail.com>
2024-03-25 03:44:41 +0800

    feat: stake reth function finished

--- src/ABI/index.ts
+++ src/ABI/index.ts
@@ -1,6 +1,10 @@
-import { LocalTokenSymbol } from '@/types/index.d';
+import { LocalTokenSymbol, ContractAddrKey } from '@/types/index.d';
+import { add } from '@/contants/address';
 import RETHABI from './RETH';
+import RETHStakeManager from './RETHStakeManager';
 
-export const TokenABIMap: Record<LocalTokenSymbol, any> = {
+export const TokenABIMap: Record<LocalTokenSymbol | ContractAddrKey, any> = {
   [LocalTokenSymbol.RETH]: RETHABI,
+  [LocalTokenSymbol.PETH]: RETHABI,
+  [ContractAddrKey.RETHStakeManager]: RETHStakeManager,
 };
--- src/app/stake/page.tsx
+++ src/app/stake/page.tsx
@@ -1,9 +1,9 @@
 "use client"
-import { Tabs, Flex, TabList, Tab, Box, Text, Container } from '@chakra-ui/react'
+import { Tabs, Flex, TabList, Tab, Box, Text, Container, localStorageManager } from '@chakra-ui/react'
 import { useEffect, useState } from 'react'
 import { useBalance, useAccount } from 'wagmi'
 import { formatUnits, formatEther } from 'viem'
-import { tabList, TabType, IIswitch, PairSelectList } from './types'
+import { tabList, TabType, SwitchState, PairSelectList, TokenPairMap, TokenMint, TokenStake } from './types'
 import { LocalTokenSymbol } from '@/types/index.d'
 import { ArrowDownIcon } from '@chakra-ui/icons'
 
@@ -15,14 +15,11 @@ import InputSelect from './components/InputSelect'
 export default function Stake () {
   const [currentTabType, setTabId] = useState<TabType>(TabType.Mint)
   const [inputValue, setInputValue] = useState<string>("0")
-  const [isSwitchCoin, setIsSwitchCoin] = useState<IIswitch>({
-    [TabType.Mint]: 0,
-    [TabType.Stake]: 0,
-  })
+  const [switchState, setSwitchState] = useState<SwitchState>(0)
   const [selectedToken, setSelectedToken] = useState<LocalTokenSymbol>(LocalTokenSymbol.ETH)
-  const [currList, setCurrList] = useState<Array<Array<LocalTokenSymbol>>>(PairSelectList[currentTabType]) 
+  const [currList, setCurrList] = useState<Array<LocalTokenSymbol>>(PairSelectList[TabType.Mint][0]) 
   const [currIndex, setCurrIndex] = useState<number>(0) 
-
+  const currTokenPairMap = TokenPairMap[currentTabType]
   const { address, isConnected} = useAccount()
   let balance = "0"
   const value = useBalance({ address }).data?.value
@@ -31,46 +28,38 @@ export default function Stake () {
     balance = formatEther(value)
     balance = parseFloat(balance).toFixed(4)
   }
+  // // 切换 Tab
+  // useEffect(() => {
+    
+  // }, [currentTabType])
+  
+  const setNewList = (newState: SwitchState) => {
+    const newList = PairSelectList[currentTabType][newState]
+    setCurrList(newList)
+  }
 
   // 点击切换Token交换顺序
   useEffect(() => {
-    const newList = currList.map((item, index) => {
-      if (item.includes(selectedToken)) {
-        [item[1], item[0]] = [item[0], item[1]]
-        setCurrIndex(index)
-      }
-      return item
-    })
-    
-    setCurrList(newList)
-  }, [isSwitchCoin[currentTabType]])
-
-  useEffect(() => {
-    setCurrList(PairSelectList[currentTabType])
-    setCurrIndex(0)
-    setIsSwitchCoin({
-      [TabType.Mint]: 0,
-      [TabType.Stake]: 0,
-    })
+    setNewList(switchState)
+    // 重置箭头切换
+    setSwitchState(0)
+    if (currentTabType === TabType.Mint) {
+      setSelectedToken(LocalTokenSymbol.ETH)
+    } else {
+      setSelectedToken(LocalTokenSymbol.RETH)
+    }
   }, [currentTabType])
 
-  useEffect(() => {
-    currList.forEach((item, index) => {
-      if (item.includes(selectedToken)) {
-        setCurrIndex(index)
-      }
-    })
-  }, [selectedToken])
-  
-  const onSwitchCoinType =() => {
-    setIsSwitchCoin({
-      ...isSwitchCoin,
-      [currentTabType]: isSwitchCoin[currentTabType] === 0 ? 1 : 0,
-    })
+ 
+
+  const onSwitch =() => {
+    const newState = switchState === 0 ? 1 : 0
+    setSwitchState(newState)
+    setNewList(newState)
+    setSelectedToken(currTokenPairMap[selectedToken])
   }
 
-  console.log('currList', currList);
-  
+
 
   return(
     <Container className="stack" color="#fff" fontSize="14px" marginTop="100px">
@@ -87,7 +76,7 @@ export default function Stake () {
           <Container>
             <Box marginTop="32px" padding="22px" backgroundColor="#1E1E38" borderRadius="12px">
               <InputSelect
-                coinSymbol={currList[currIndex][0]}
+                selectedToken={selectedToken}
                 currList={currList}
                 inputValue={inputValue}
                 onChangeInput={(value) => setInputValue(value)}
@@ -98,16 +87,16 @@ export default function Stake () {
                 account={address}
                 isConnected={isConnected}
                 balance={balance}
-                coinSymbol={currList[currIndex][0]}
+                selectedToken={selectedToken}
                 setMintValue={(value) => setInputValue(value)} ></BalanceCoin>
             </Box>
 
             <Flex justifyContent="center" marginTop="-12px">
-              <ArrowDownIcon onClick={() => onSwitchCoinType()} boxSize={8} w={6} cursor="pointer" _hover={{ opacity: '0.5' }} />
+              <ArrowDownIcon onClick={() => onSwitch()} boxSize={8} w={6} cursor="pointer" _hover={{ opacity: '0.5' }} />
             </Flex>
             
             {/* 带价格的 coin */}
-            <PriceCoin coinSymbol={currList[currIndex][1]}></PriceCoin>
+            <PriceCoin selectedTokenPair={currTokenPairMap[selectedToken]}></PriceCoin>
 
             <Box marginTop="32px" padding="16px" backgroundColor="rgb(52 30 56 / 50%)" borderRadius="12px">
               <Flex justifyContent="space-between" marginTop="12px">
@@ -125,11 +114,11 @@ export default function Stake () {
             </Box>
             
             <StakeButton 
-              coinSymbol={currList[currIndex][1]}
+              switchState={switchState}
+              selectedToken={selectedToken}
               account={address}
               inputValue={inputValue}
               isConnected={isConnected} 
-              isSwitchCoin={isSwitchCoin} 
               currentTabType={currentTabType}></StakeButton>
           </Container>
         </Tabs>
