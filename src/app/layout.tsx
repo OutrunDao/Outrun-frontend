a18d1beaf0e634ad027f23425e87086ec854e4fc
L啊 <liaoyux@gmail.com>
2024-03-31 21:18:11 +0800

    fixed build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,18 @@
-import { Inter } from "next/font/google";
-import { Providers } from "./providers";
-import PageLayout from "@/components/PageLayout";
-import Web3ContextProvider from "@/context/web3modal";
-import { config as web3Config } from "@/config/web3modal";
-import { cookieToInitialState } from "wagmi";
-import { headers } from "next/headers";
-const initialState = cookieToInitialState(web3Config, headers().get("cookie"));
+import { Inter } from 'next/font/google';
+import { Providers } from './providers';
+import PageLayout from '@/components/PageLayout';
+import Web3ContextProvider from '@/context/web3modal';
+import { config as web3Config } from '@/config/web3modal';
+import { cookieToInitialState } from 'wagmi';
+import { headers } from 'next/headers';
 
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Outrun App",
-  description: "Outrun app",
+  title: 'Outrun App',
+  description: 'Outrun app',
 };
 
 export default function RootLayout({
@@ -21,6 +20,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const initialState = cookieToInitialState(web3Config, headers().get('cookie'));
   return (
     <html lang="en">
       <body className={inter.className}>
