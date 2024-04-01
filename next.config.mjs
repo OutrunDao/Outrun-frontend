d1a2efa41cc2c5feb6b4a003a496d7e0dbf4fca9
L啊 <liaoyux@gmail.com>
2024-04-01 10:26:43 +0800

    ignore graphprotocol build warnning

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    // ignore graphprotocol build warnning
+    // see: https://github.com/graphprotocol/graph-client/issues/303
+    config.module.exprContextCritical = false;
+    return config;
+  },
+};
 
 export default nextConfig;
