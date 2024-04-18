222d6d83db819960221d3d91b4e79564aa885506
L啊 <liaoyux@gmail.com>
2024-04-18 12:46:06 +0800

    feat: launchpad style

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,11 +1,17 @@
 'use client';
-import { Box, Flex, Spacer, Text, Button, HStack, Heading, Image, Container } from '@chakra-ui/react';
+import { Box, Flex, Spacer, Text, Button, HStack, Heading, Image, Icon, Container } from '@chakra-ui/react';
 import { Link } from '@chakra-ui/next-js';
 import { Silkscreen } from 'next/font/google';
+import { IoSwapVertical } from 'react-icons/io5';
+import { PiSwimmingPool } from 'react-icons/pi';
+import { GiMiner } from 'react-icons/gi';
+import { FaHotjar } from 'react-icons/fa';
+import { usePathname } from 'next/navigation';
 
 const silk = Silkscreen({ subsets: ['latin'], weight: '700' });
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
   return (
     <Box>
       <Flex align="center" borderColor="gray.600" borderBottomStyle={'solid'} borderBottomWidth={'0.5px'}>
@@ -16,16 +22,40 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         </Flex>
         <Box ml={'60px'}>
           <HStack spacing={'40px'} fontSize={14}>
-            <Button variant="link" color="grey.600" fontSize={14} fontWeight={'normal'}>
+            <Button
+              variant="link"
+              color={pathname === '/swap' ? 'brand.500' : 'grey.600'}
+              fontSize={14}
+              fontWeight={'normal'}
+              leftIcon={<Icon fontSize={16} as={IoSwapVertical}></Icon>}
+            >
               <Link href={'/swap'}>Swap</Link>
             </Button>
-            <Button variant="link" color="grey.600" fontSize={14} fontWeight={'normal'}>
+            <Button
+              variant="link"
+              color={pathname === '/pool' ? 'brand.500' : 'grey.600'}
+              fontSize={14}
+              fontWeight={'normal'}
+              leftIcon={<Icon fontSize={16} as={PiSwimmingPool}></Icon>}
+            >
               <Link href={'/pool'}>Pool</Link>
             </Button>
-            <Button variant="link" color="grey.600" fontSize={14} fontWeight={'normal'}>
+            <Button
+              variant="link"
+              color={pathname === '/stake' ? 'brand.500' : 'grey.600'}
+              fontSize={14}
+              fontWeight={'normal'}
+              leftIcon={<Icon fontSize={16} as={GiMiner}></Icon>}
+            >
               <Link href={'/stake'}>Stake</Link>
             </Button>
-            <Button variant="link" color="grey.600" fontSize={14} fontWeight={'normal'}>
+            <Button
+              variant="link"
+              color={pathname === '/launchpad' ? 'brand.500' : 'grey.600'}
+              fontSize={14}
+              fontWeight={'normal'}
+              leftIcon={<Icon fontSize={16} as={FaHotjar}></Icon>}
+            >
               <Link href={'/launchpad'}>Launchpad</Link>
             </Button>
           </HStack>
--- src/views/launchpad/page.tsx
+++ src/views/launchpad/page.tsx
@@ -21,8 +21,15 @@ import {
   TabPanel,
   TabPanels,
   Divider,
+  Image,
+  Flex,
+  Tag,
+  HStack,
+  SimpleGrid,
 } from '@chakra-ui/react';
 import { useAccount, useChainId, usePublicClient, useWalletClient } from 'wagmi';
+import { FaXTwitter } from 'react-icons/fa6';
+import { FaDiscord, FaInternetExplorer } from 'react-icons/fa';
 
 const Launchpad = () => {
   const chainId = useChainId();
@@ -37,15 +44,82 @@ const Launchpad = () => {
         <VStack>
           <Heading as={'h2'}>The most equitable and fair token launch platform</Heading>
           <br />
-          <Heading as="h5" size={'sm'}>
-            Discover new, high-quality projects around the world
+          <Heading as="h5" size={'sm'} mt="10">
+            Enjoy high-quality projects around the world
           </Heading>
           <br />
-          <Button colorScheme="gray">
+          {/* <Button colorScheme="gray">
             <Link href={'/pool/create'}>Discover Project</Link>
-          </Button>
+          </Button> */}
         </VStack>
       </Box>
+      <Container
+        maxW={'container.lg'}
+        borderStyle={'solid'}
+        borderWidth={'0.5px'}
+        borderRadius={'md'}
+        borderColor="gray.600"
+        boxShadow="xs"
+        rounded="md"
+        p={6}
+        mt="24"
+      >
+        <Flex>
+          <Center width={'200px'} p="10">
+            <VStack spacing={'4'}>
+              <Image alt="logo" width={100} height={100} src="https://ethena.fi/shared/ethena.svg"></Image>
+            </VStack>
+          </Center>
+          <VStack alignItems={'flex-start'}>
+            <Link href="/launchpad/1">
+              <HStack spacing={'2'}>
+                <Heading as={'h6'} fontSize={'18px'}>
+                  Ethena
+                </Heading>
+                <Tag size={'sm'}>Staking</Tag>
+                <Tag size={'sm'}>Layer2</Tag>
+              </HStack>
+            </Link>
+            <Link href="/launchpad/1">
+              <Text>
+                Ethena is a synthetic dollar protocol built on Ethereum that provides a crypto-native solution
+                for money not reliant on traditional banking system infrastructure, alongside a globally
+                accessible dollar denominated instrument - the Internet Bond.
+              </Text>
+            </Link>
+
+            <HStack spacing={'2'}>
+              <Link href={'#'}>
+                <Icon as={FaXTwitter} />
+              </Link>
+              <Link href="#">
+                <Icon as={FaDiscord} />
+              </Link>
+              <Link href="#">
+                <Icon as={FaInternetExplorer} />
+              </Link>
+            </HStack>
+            <SimpleGrid columns={2} spacing={2} fontSize={'14px'}>
+              <HStack spacing={'6'}>
+                <Text>Token</Text>
+                <Text color={'gray'}>ENA</Text>
+              </HStack>
+              <HStack spacing={'6'}>
+                <Text>Amount</Text>
+                <Text color={'gray'}>10000000</Text>
+              </HStack>
+              <HStack spacing={'6'}>
+                <Text>Price</Text>
+                <Text color={'gray'}>0.00000001</Text>
+              </HStack>
+              <HStack spacing={'6'}>
+                <Text>Launch Date</Text>
+                <Text color={'gray'}>2024-04-16 18:00</Text>
+              </HStack>
+            </SimpleGrid>
+          </VStack>
+        </Flex>
+      </Container>
     </Box>
   );
 };
